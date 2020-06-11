b49ddb4a00528983fe5c3c61ef6ae49196e19530
Kiran P <pkiran.kala@gmail.com>
2020-06-11 09:11:20 +0530

    Using flask textbox to take input and send it to the chatbot

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
 from app import app
+from flask import request, render_template, jsonify
+from chatbot import ask_bot
 
-@app.route("/")
+
 @app.route("/index")
 def index():
     return "Welcome to Doc Chat Bot!!!"
+
+
+@app.route("/")
+def query_form():
+    return render_template("query_form.html")
+
+@app.route("/", methods=['POST'])
+def query_form_post():
+    text =  request.form['text']
+    processed_text = text.upper()
+    response = ask_bot(processed_text)
+    return jsonify(response) 
